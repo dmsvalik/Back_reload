d50d978f78177cde578665d909c38cd04e3dd099
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-03-31 13:34:42 +0300

    pep8 admin.py

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import UserAccount
 
+
 @admin.register(UserAccount)
-class UserAccount_admin(admin.ModelAdmin):
+class UserAccountAdmin(admin.ModelAdmin):
     list_display = ['email', 'name', 'is_active', 'is_staff']
