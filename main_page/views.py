ad730839d7e42336c3a7eff8352948ad8d6ed6f4
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-05-03 00:06:38 +0300

    Swagger documentation on PersonalClientApi

--- main_page/views.py
+++ main_page/views.py
@@ -7,11 +7,37 @@ from .serializers import PersonalClientSerializer
 
 
 class PersonalClientAPIView(APIView):
+
     def get(self, request):
+        """
+        Запрос на получение данных (телефон, имя, адрес...)
+
+        ---
+        :parameter
+        - вернуться данные конкретно на пользователя токена JWT
+        """
         p = PersonalClientData.objects.all()
         return Response({'personal_data': PersonalClientSerializer(p, many=True).data})
 
     def post(self, request):
+        """
+        Сохранение новых данных (телефон, имя, адрес...)
+
+        ---
+        - 'person_telephone': 89117861593
+          required: true
+          type: string
+
+        - 'person_name': Sam
+          required: true
+          type: string
+
+        - 'person_address': Rusia
+          required: true
+          type: string
+
+        """
+
         username = request.user
 
         post_new = PersonalClientData.objects.create(
