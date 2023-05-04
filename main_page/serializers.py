cd3cc2a3ac10c34fe58b9925eb89dd385e1a227a
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-05-04 19:50:34 +0300

    BIG delete PersonaDataTable logic, all in user model;

--- main_page/serializers.py
+++ main_page/serializers.py
@@ -1,29 +1,9 @@
 from rest_framework import serializers
-from .models import PersonalClientData
-
 from django.contrib.auth import get_user_model
-from djoser.serializers import UserCreateSerializer
 
 User = get_user_model()
 
-
-class UserCreateSerializer(UserCreateSerializer):
+class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'name', 'password')
-
-class PersonalClientSerializer(serializers.Serializer):
-    person_telephone = serializers.CharField()
-    person_name = serializers.CharField()
-    person_address = serializers.CharField()
-
-    def create(self, validated_data):
-        return PersonalClientData.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.person_telephone = validated_data.get('person_telephone', instance.person_telephone)
-        instance.person_name = validated_data.get('person_name', instance.person_name)
-        instance.person_address = validated_data.get('person_address', instance.person_address)
-        instance.save()
-        return instance
-
+        fields = ('id', 'email', 'name', 'password', 'person_telephone', 'surname')
--- main_page/urls.py
+++ main_page/urls.py
@@ -1,11 +1,8 @@
 from django.conf import settings
-from django.contrib.auth.views import LogoutView
 from django.urls import include, path
-from .views import PersonalClientAPIView
 
 from . import views
 
 urlpatterns = [
-    path('api/personal_client_data/', PersonalClientAPIView.as_view()),
-    path('api/personal_client_data/<int:pk>', PersonalClientAPIView.as_view()),
+
 ]
