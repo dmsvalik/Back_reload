2998ac475e5bc2a0d75a134f93b332baf966a005
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-05-15 09:51:58 +0300

    done reset password link and logic

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
-from .views import FeedbackViewSet, ActivateUser
+from .views import FeedbackViewSet, ActivateUser, reset_password
 
 from rest_framework import routers
 router = routers.SimpleRouter()
@@ -10,4 +10,5 @@ router.register(r'feedback', FeedbackViewSet)
 urlpatterns = [
     path('users/', include(router.urls)),
     path('activate/<uid>/<token>', ActivateUser.as_view({'get': 'activation'}), name='activation'),
+    path('password/reset/confirm/<str:uid>/<str:token>', reset_password, name='reset_password'),
 ]
