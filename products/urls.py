b27ae5e83f0d9861e38dbd7d2869c02365145114
ilshat <maddust2002@gmail.com>
2023-05-15 23:18:27 +0300

    add createview for product

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
-from .views import CardModelAPIView, CategoryModelListAPIView
+from .views import CardModelAPIView, CategoryModelListAPIView, ProductModelCreateAPIView
 
 
 urlpatterns = [
     path('products/all_CardModel/', CardModelAPIView.as_view()),
-    path('products/card_categories/<int:card_id>', CategoryModelListAPIView.as_view())
+    path('products/card_categories/<int:card_id>', CategoryModelListAPIView.as_view()),
+    path('products/product', ProductModelCreateAPIView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import BasePermission, IsAuthenticated, AllowAny
 
 from .models import CardModel, CategoryModel
-from .serializers import CardModelSerializer, CategoryModelSeializer
+from .serializers import CardModelSerializer, CategoryModelSeializer, ProductModelCreateSerializer
 
 
 class CardModelAPIView(APIView):
@@ -22,13 +22,22 @@ class CardModelAPIView(APIView):
 
 class CategoryModelListAPIView(ListAPIView):
     '''
-        Получить список категорий по id карточки
+    Получить список категорий по id карточки
 
     '''
     model = CategoryModel
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     serializer_class = CategoryModelSeializer
 
     def get_queryset(self):
         card_id = self.kwargs['card_id']
         return CategoryModel.objects.filter(card__id=card_id).all()
+
+
+class ProductModelCreateAPIView(CreateAPIView):
+    '''
+    Создать продукт заказа
+
+    '''
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProductModelCreateSerializer
