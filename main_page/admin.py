df2cd353e6a6e94f6761abaad0942d74712d46fb
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-05-18 10:06:44 +0300

    done - new model SellerData + admin

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from .models import UserAccount
+from .models import UserAccount, SellerData
 
 
 @admin.register(UserAccount)
 class UserAccountAdmin(admin.ModelAdmin):
     list_display = ['email', 'name', 'is_active', 'is_staff']
 
+@admin.register(SellerData)
+class SellerDataAdmin(admin.ModelAdmin):
+    list_display = ['user_account_id', 'seller_name_company', 'seller_activity', 'seller_telephone']
