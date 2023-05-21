ea56218941bdc84a911970fd7bd0644769adb426
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-05-21 13:39:24 +0300

    reparing models product

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@ from main_page.models import UserAccount
 
 
 STATE_CHOICES = (
+    ('creating', 'Создание заказа'),
     ('auction', 'Аукцион'),
     ('new', 'Новый'),
     ('confirmed', 'Подтвержден'),
@@ -16,7 +17,6 @@ STATE_CHOICES = (
 class OrderModel(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     user_account = models.ForeignKey(UserAccount, on_delete=models.CASCADE, null=True)
-    card_id = models.ForeignKey('products.CardModel', on_delete=models.CASCADE, null=False,  blank=True)
     order_time = models.DateTimeField('Дата создания заказа', auto_now=True)
     order_description = models.CharField('Описание заказа', max_length=300, blank=True)
     state = models.CharField(verbose_name='Статус', choices=STATE_CHOICES, max_length=15)
