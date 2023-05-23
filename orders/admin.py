2afbe5e9bab636582c0e7364bd147b9172731d1d
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-05-23 20:47:28 +0300

    Done orderoffer

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import OrderModel, OrderImageModel
+from .models import OrderModel, OrderImageModel, OrderOffer
 
 
 @admin.register(OrderModel)
 class OrderModelAdmin(admin.ModelAdmin):
     list_display = ['order_time', 'id', 'state', 'user_account']
 
+
 @admin.register(OrderImageModel)
 class OrderImageModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'order_id']
+
+
+@admin.register(OrderOffer)
+class OrderOfferAdmin(admin.ModelAdmin):
+    list_display = ['offer_create_at', 'id', 'order_id']
--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,13 @@ urlpatterns = [
     path('products/image', OrderImageViewSet.as_view({
         'get': 'list',
         'post': 'create'})),
+
+    path('offer/', OrderOfferViewSet.as_view({
+        'get': 'list',
+        'post': 'create'})),
+
+    path('offer/<int:pk>', OrderOfferViewSet.as_view({
+        'get': 'retrieve',
+        'delete': 'destroy',
+        'put': 'update'})),
 ]
