18f1a7122428c9ff5f44636da8d67226e9b52a7f
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-06-12 15:45:04 +0300

    restart pre-main

--- main_page/email.py
+++
@@ -1,9 +0,0 @@
-from djoser import email
-
-
-class Activation(email.ActivationEmail):
-    template_name = 'email/activation.html'
-
-
-class Confirmation(email.ConfirmationEmail):
-    template_name = 'email/confirmation.html'
