dbdaa3dc97b9542a4cdc365694edbe319ffd1f23
ilshat <maddust2002@gmail.com>
2023-06-15 23:37:45 +0300

    add api for questions

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from .views import CardModelAPIView, CategoryModelListAPIView, CategoryModelListAPIView, ProductModelCreateAPIView, \
-    ProductModelAPIView
-
+    ProductModelAPIView, QuestionsModelListAPIView
 
 urlpatterns = [
     path('products/all_CardModel/', CardModelAPIView.as_view()),
     path('products/card_categories/<int:card_id>', CategoryModelListAPIView.as_view()),
+    path('products/category/<int:category_id>/questions', QuestionsModelListAPIView.as_view()),
     path('products/product', ProductModelCreateAPIView.as_view()),
 
     path('products/<int:pk>', ProductModelAPIView.as_view({
