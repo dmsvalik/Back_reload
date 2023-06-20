14a927be418fb3cc4188921ce2660b6d500b695c
Андрей Распутин <rasputin21@gmail.com>
2023-06-20 21:13:04 +0500

    test_unauthorized_request

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,7 @@
 import pytest
 from django.contrib.auth import get_user_model
 from django.core import mail
+from django.urls import reverse
 
 
 User = get_user_model()
@@ -14,7 +15,7 @@ class TestUserRegistration:
     url_create_user = '/auth/users/'
 
     @pytest.mark.django_db(transaction=True)
-    def test_valid_data_create_user(self, client):
+    def test_valid_data_create_user(self, api_client):
         """Тест создания пользователя с валидными данными."""
         outbox_before_count = len(mail.outbox)
         valid_data = {
@@ -25,7 +26,7 @@ class TestUserRegistration:
             'surname': 'TestUser',
         }
         request_type = 'POST'
-        response = client.post(self.url_create_user, data=valid_data)
+        response = api_client.post(self.url_create_user, data=valid_data)
         outbox_after = mail.outbox
         assert response.status_code != 404, (
             f'Страница `{self.url_create_user}` не найдена, проверьте этот адрес в *urls.py*'
@@ -33,7 +34,7 @@ class TestUserRegistration:
         code = 201
         assert response.status_code == code, (
             f'Проверьте, что при {request_type} запросе `{self.url_create_user}` с валидными данными '
-            f'от имени администратора, создается пользователь и возвращается статус {code}'
+            f'создается пользователь и возвращается статус {code}'
         )
         response_json = response.json()
         for field in valid_data:
@@ -41,15 +42,20 @@ class TestUserRegistration:
                 continue
             assert field in response_json and valid_data.get(field) == response_json.get(field), (
                 f'Проверьте, что при {request_type} запросе `{self.url_create_user}` с валидными данными '
-                f'от имени администратора, в ответ приходит созданный объект пользователя в виде словаря'
+                f'в ответ приходит созданный объект пользователя в виде словаря'
             )
         new_user = User.objects.filter(email=valid_data['email'])
         assert new_user.exists(), (
             f'Проверьте, что при {request_type} запросе `{self.url_create_user}` с валидными данными '
-            f'от имени администратора, в БД создается пользователь и возвращается статус {code}'
+            f'в БД создается пользователь и возвращается статус {code}'
         )
         assert len(outbox_after) == outbox_before_count + 1, (
             f'Проверьте, что при {request_type} запросе `{self.url_create_user}` с валидными данными '
-            f'от имени администратора, пользователю отправляется email с кодом подтверждения'
+            f'пользователю отправляется email с кодом подтверждения'
         )
         new_user.delete()
+
+    @pytest.mark.django_db
+    def test_unauthorized_request(self, api_client):
+        response = api_client.get(self.url_create_user)
+        assert response.status_code == 401
