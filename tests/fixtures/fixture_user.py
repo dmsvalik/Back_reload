15f8154d72b1ac569b17513d9149cbd8b7fb585c
Андрей Распутин <rasputin21@gmail.com>
2023-06-21 19:27:18 +0500

    Add some tests to TestUserRegistration

--- tests/fixtures/fixture_user.py
+++ tests/fixtures/fixture_user.py
@@ -12,7 +12,51 @@ def user_superuser(django_user_model):
     return django_user_model.objects.create_superuser(
         email='testsuperuser@project.fake',
         name='TestSuperuser',
-        password='12345678',
-        person_telephone='9000000000',
+        password='32[fN_r7/z5v`o&N',
+        person_telephone='+79000000000',
         surrname='TestSuperuser',
     )
+
+
+url_signup = '/auth/users/'
+url_token = '/auth/jwt/create/'
+user_full_data_1 = {
+    'email': 'fulluser@mail.fake',
+    'name': 'TestUser',
+    'password': '_>Ke:[Bs<kSo[H9T',
+    'person_telephone': '+79000000000',
+    'surname': 'TestUseroff',
+}
+user_full_data_2 = {
+    'email': 'fulluser2@mail.fake',
+    'name': 'TestUser',
+    'password': 'C1Pt}&Rq*4^!A*)A',
+    'person_telephone': '89000000000',
+    'surname': 'TestUseroff',
+}
+user_full_data_3 = {
+    'email': 'fulluser3@mail.fake',
+    'name': 'TestUser',
+    'password': '>-Kc-k]_1UwVy1>^',
+    'person_telephone': '9000000000',
+    'surname': 'TestUseroff',
+}
+user_minimum_data = {
+    'email': 'minuser@mail.fake',
+    'name': 'MinUserName',
+    'password': 's.GD4bBXo#;JS#Dt',
+}
+user_invalid_data_1 = {
+    'email': 'invalid_email',
+    'name': '123',
+    'password': '1234578',
+    'person_telephone': '8900',
+    'surname': '321',
+}
+user_invalid_data_2 = {
+    'email': 'invalid@email',
+    'name': '@!=+-',
+    'password': '!@#',
+    'person_telephone': '0000000000',
+    'surname': '-+=!@',
+}
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,10 @@
 import pytest
 from django.contrib.auth import get_user_model
 from django.core import mail
-from django.urls import reverse
 
+from tests.fixtures.fixture_user import (
+    user_full_data_1, user_invalid_data_1, user_invalid_data_2, user_full_data_2, user_full_data_3, url_signup
+)
 
 User = get_user_model()
 
@@ -10,52 +12,129 @@ pytestmark = pytest.mark.users
 
 
 class TestUserRegistration:
-    # url_signup = '/auth/users/'
-    # url_token = '/auth/jwt/create/'
-    url_create_user = '/auth/users/'
+    """Тестирование регистрации пользователей."""
 
     @pytest.mark.django_db(transaction=True)
-    def test_valid_data_create_user(self, api_client):
-        """Тест создания пользователя с валидными данными."""
+    @pytest.mark.parametrize(
+        "valid_data", [
+            # user_minimum_data,
+            user_full_data_1,
+            user_full_data_2,
+            user_full_data_3,
+        ]
+    )
+    def test_valid_data_user_signup(self, api_client, valid_data):
+        """Тест регистрации пользователя с валидными данными."""
         outbox_before_count = len(mail.outbox)
-        valid_data = {
-            'email': 'testuser@project.fake',
-            'name': 'TestUser',
-            'password': '12345678',
-            'person_telephone': '9000000000',
-            'surname': 'TestUser',
-        }
         request_type = 'POST'
-        response = api_client.post(self.url_create_user, data=valid_data)
+        response = api_client.post(url_signup, data=valid_data)
         outbox_after = mail.outbox
         assert response.status_code != 404, (
-            f'Страница `{self.url_create_user}` не найдена, проверьте этот адрес в *urls.py*'
+            f'Эндпойнт `{url_signup}` не найден, проверьте этот адрес в *urls.py*'
+        )
+        assert response.status_code != 400, (
+            f'Проверьте, что при {request_type} запросе `{url_signup}` с валидными данными '
+            f'передаются все обязательные поля'
         )
         code = 201
         assert response.status_code == code, (
-            f'Проверьте, что при {request_type} запросе `{self.url_create_user}` с валидными данными '
-            f'создается пользователь и возвращается статус {code}'
+            f'Проверьте, что при {request_type} запросе `{url_signup}` с валидными данными '
+            f'создаётся пользователь и возвращается статус {code}'
         )
         response_json = response.json()
         for field in valid_data:
             if field == 'password':
                 continue
             assert field in response_json and valid_data.get(field) == response_json.get(field), (
-                f'Проверьте, что при {request_type} запросе `{self.url_create_user}` с валидными данными '
+                f'Проверьте, что при {request_type} запросе `{url_signup}` с валидными данными '
                 f'в ответ приходит созданный объект пользователя в виде словаря'
             )
         new_user = User.objects.filter(email=valid_data['email'])
         assert new_user.exists(), (
-            f'Проверьте, что при {request_type} запросе `{self.url_create_user}` с валидными данными '
+            f'Проверьте, что при {request_type} запросе `{url_signup}` с валидными данными '
             f'в БД создается пользователь и возвращается статус {code}'
         )
         assert len(outbox_after) == outbox_before_count + 1, (
-            f'Проверьте, что при {request_type} запросе `{self.url_create_user}` с валидными данными '
+            f'Проверьте, что при {request_type} запросе `{url_signup}` с валидными данными '
             f'пользователю отправляется email с кодом подтверждения'
         )
         new_user.delete()
 
     @pytest.mark.django_db
     def test_unauthorized_request(self, api_client):
-        response = api_client.get(self.url_create_user)
-        assert response.status_code == 401
+        """Тест отсутствия доступа неавторизованным пользователям."""
+        response = api_client.get(url_signup)
+        response_type = 'GET'
+        assert response.status_code == 401, (
+            f'Проверьте, что при {response_type} запросе `{url_signup}` неавторизованным пользователям '
+            f'доступ запрещён'
+        )
+
+    @pytest.mark.skip(reason='Разобраться с сериализаторами регистрации пользователей - обязательные поля!')
+    @pytest.mark.django_db(transaction=True)
+    def test_nodata_signup(self, client):
+        """Тест попытки регистрации с пустыми полями."""
+        request_type = 'POST'
+        response = client.post(url_signup)
+
+        assert response.status_code != 404, (
+            f'Эндпойнт `{url_signup}` не найден, проверьте этот адрес в *urls.py*'
+        )
+        code = 400
+        assert response.status_code == code, (
+            f'Проверьте, что при {request_type} запросе `{url_signup}` без параметров '
+            f'не создается пользователь и возвращается статус {code}'
+        )
+        response_json = response.json()
+        empty_fields = ['email', 'name', 'password', 'person_telephone', 'surname']
+        for field in empty_fields:
+            assert (field in response_json.keys()
+                    and isinstance(response_json[field], list)), (
+                f'Проверьте, что при {request_type} запросе `{url_signup}` без параметров '
+                f'в ответе есть сообщение о том, какие поля не заполнены'
+            )
+
+    @pytest.mark.skip(reason='Добавить валидацию всех полей')
+    @pytest.mark.django_db(transaction=True)
+    @pytest.mark.parametrize(
+        "invalid_data", [
+            user_invalid_data_1,
+            user_invalid_data_2,
+        ]
+    )
+    def test_invalid_data_signup(self, client, invalid_data):
+        """Тест регистрации пользователей с невалидными данными."""
+        request_type = 'POST'
+        response = client.post(url_signup, data=invalid_data)
+
+        code = 400
+        assert response.status_code == 400, (
+            f'Проверьте, что при {request_type} запросе `{url_signup}` с невалидными данными - '
+            f'не создается пользователь и возвращается статус {code}'
+        )
+
+        response_json = response.json()
+        invalid_fields = ['email', 'name', 'password', 'person_telephone', 'surname']
+        for field in invalid_fields:
+            assert (field in response_json.keys()
+                    and isinstance(response_json[field], list)), (
+                f'Проверьте, что при {request_type} запросе `{url_signup}` с невалидными данными - '
+                f'в ответе есть сообщение о том, какие поля заполнены неправильно'
+            )
+
+    @pytest.mark.django_db(transaction=True)
+    @pytest.mark.parametrize(
+        "valid_data", [
+            user_full_data_1,
+        ]
+    )
+    def test_same_email_user_signup(self, client, valid_data):
+        """Тест повторной регистрации пользователя."""
+        request_type = 'POST'
+        client.post(url_signup, data=valid_data)
+        response = client.post(url_signup, data=valid_data)
+        code = 400
+        assert response.status_code == code, (
+            f'Проверьте, что при {request_type} запросе `{url_signup}` нельзя создать '
+            f'пользователя, email которого уже зарегистрирован, и возвращается статус {code}'
+        )
