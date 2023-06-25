fff1f61c6fa77c8115e430f73576c74499262038
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-06-26 00:58:30 +0300

    done urls rename

--- products/urls.py
+++ products/urls.py
@@ -5,14 +5,12 @@ from .views import (AnswerListAPIView, CardModelAPIView,
 
 
 urlpatterns = [
-    path("products/all_CardModel/", CardModelAPIView.as_view()),
-    path("products/card_categories/<int:card_id>", CategoryModelListAPIView.as_view()),
+    path("products/step_1/card/", CardModelAPIView.as_view()),
+    path("products/step_2/<int:card_id>", CategoryModelListAPIView.as_view()),
     path(
-        "products/category/<int:category_id>/questions",
+        "products/step_3_1/<int:category_id>/questions",
         QuestionsModelListAPIView.as_view(),
     ),
-    path("products/questions_response", AnswerListAPIView.as_view()),
-    path("products/step4/create_order", CreateOrderAnswers),
-
-
+    path("products/step_3_2/questions_response", AnswerListAPIView.as_view()),
+    path("products/step_4/create_order", CreateOrderAnswers),
 ]
