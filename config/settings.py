df1f6108d2b6ef9cc1514403dc1d49444413bd6e
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-06-28 00:57:23 +0300

    task/fix_email every time when we update user + added installed aps - authtoken

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework.authtoken",
+
     "main_page",
     "products",
     "orders",
