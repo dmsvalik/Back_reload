ff6f9957d90260df8b735e9ff120029a7cd79c04
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-07-15 13:38:12 +0300

    corse new settings 2

--- config/settings.py
+++ config/settings.py
@@ -63,22 +63,6 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1",
-    "http://127.0.0.1:3000",
-    "http://localhost",
-    "http://localhost:3000",
-    "http://app",
-    "http://185.244.173.82",
-]
-
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    "http://app",
-    "http://185.244.173.82",
-]
-
 CORS_ALLOW_METHODS = [
     'DELETE',
     'GET',
@@ -99,6 +83,25 @@ CORS_ALLOW_HEADERS = [
     "x-csrftoken",
     "x-requested-with",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+    "http://localhost",
+    "http://localhost:3000",
+    "http://app",
+    "http://185.244.173.82",
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    "http://app",
+    "http://185.244.173.82",
+]
+
 
 ROOT_URLCONF = "config.urls"
 
