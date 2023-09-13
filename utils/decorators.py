8baf55aa1e3bbbeecbb68330fdd53d8622e9fd86
Яна Курзюкова <kurzukova.yana@yandex.ru>
2023-09-13 16:28:40 +0300

    add 2 decorators

---
+++ utils/decorators.py
@@ -0,0 +1,35 @@
+from functools import wraps
+from django.http import HttpResponse
+from .models import UserQuota
+
+
+def check_user_quota(func):
+    @wraps(func)
+    def wrapped(request, *args, **kwargs):
+        user_id = request.user.id
+        try:
+            user_quota = UserQuota.objects.get(user_id=user_id)
+        except UserQuota.DoesNotExist:
+            return HttpResponse("Пользователь с указанным ID не найден", status=404)
+        if user_quota.quota_cloud_size >= 3 * 1024 * 1024:
+            return HttpResponse("Превышена квота в облаке (3 МБ). Удалите лишние файлы", status=400)
+        if user_quota.total_server_size >= 5 * 1024 * 1024:
+            return HttpResponse("Превышена квота на сервере (5 МБ). Удалите лишние файлы", status=400)
+        if user_quota.total_traffic < 0:
+            return HttpResponse("Превышен допустимый трафик. Дождитесь следующего месяца или обратитесь к администраторам", status=404)
+        return func(request, *args, **kwargs)
+    return wrapped
+
+
+def check_file_type(allowed_types):
+    def decorator(func):
+        @wraps(func)
+        def wrapped(request, *args, **kwargs):
+            uploaded_file = request.FILES.get('file')
+            if uploaded_file:
+                file_extension = uploaded_file.name.split('.')[-1].lower()
+                if file_extension not in allowed_types:
+                    return HttpResponse("Недопустимый тип файла", status=400)
+            return func(request, *args, **kwargs)
+        return wrapped
+    return decorator
--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from main_page.models import UserAccount
 
-# Create your models here.
+class UserQuota(models.Model):
+    user = models.OneToOneField(UserAccount, on_delete=models.CASCADE)
+    quota_cloud_size = models.PositiveIntegerField(default=20)
+    total_server_size = models.PositiveIntegerField(default=5) 
+    total_traffic = models.PositiveIntegerField(default=1000)  
+
+    def reset_traffic(self):
+        self.total_traffic = 0
+        self.save()
