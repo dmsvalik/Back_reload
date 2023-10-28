c8d3abb7b6e9f0cb26f4480349b1d29a3bae1473
Aleksandr140590 <alex2717@yandex.kz>
2023-10-29 01:43:28 +0600

    fix/jenkins_deploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
 done
