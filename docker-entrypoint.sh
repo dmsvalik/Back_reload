5d6e35cebbfdd2d530d541b18d2afb5ab2f4d15f
Aleksandr140590 <alex2717@yandex.kz>
2023-10-29 01:32:54 +0600

    fix/jenkins_deploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
 poetry run task migrate
 poetry run task collectstatic
 poetry run task createsuperuser --noinput
