99c03c39f0ce1988aa76372f03d5efe0cb89b60d
Aleksandr140590 <alex2717@yandex.kz>
2023-10-29 01:54:56 +0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    sleep 1
-done
-
->&2 echo "Postgres is up - executing command"
-
 poetry run task migrate
 poetry run task collectstatic
 poetry run task createsuperuser --noinput
