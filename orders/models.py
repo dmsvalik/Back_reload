301f1ec067e2d1423553995a2efb525315bad62e
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2023-11-10 00:11:46 +0300

    fix order models

--- orders/models.py
+++ orders/models.py
@@ -20,7 +20,7 @@ class OrderModel(models.Model):
     order_time = models.DateTimeField("Дата создания заказа", auto_now_add=True)
     name = models.CharField("Название заказа", max_length=150, null=True)
     order_description = models.CharField("Описание заказа", max_length=300, blank=True)
-    card_category = models.ForeignKey("products.Category", on_delete=models.CASCADE, null=True)
+    card_category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     state = models.CharField(verbose_name="Статус", choices=STATE_CHOICES, max_length=50, default="draft")
 
     contractor_selected = models.ForeignKey(ContractorData, on_delete=models.SET_NULL, null=True, blank=True)
