6028bc9317dcceb2b63ec001da88747e8b5fe2f0
Aleksandr140590 <alex2717@yandex.kz>
2023-12-01 09:54:51 +0600

    fix_cors

--- config/settings.py
+++ config/settings.py
@@ -101,7 +101,7 @@ CORS_ALLOW_HEADERS = [
     "x-requested-with",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1",
@@ -110,6 +110,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://app",
     "http://185.244.173.82",
+    "https://front-git-feat-order-whywe.vercel.app",
 ]
 
 CORS_ORIGIN_WHITELIST = [
