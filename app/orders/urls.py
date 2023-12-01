cd6ee11cda00d971bec83ec10c5d84e8cf310be7
Вячеслав Мельник <jurcenter@yandex.ru>
2023-12-01 19:38:00 +0300

    add url for delete_file_order

--- app/orders/urls.py
+++ app/orders/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path("order/image_get_order/<int:file_id>", views.get_file_order, name="get-image-order"),
     path("order/client/all_orders", AllOrdersClientViewSet.as_view({"get": "list"})),
     path("order/client/archive", ArchiveOrdersClientViewSet.as_view({"get": "list"})),
+    path("order/delete_file_order/", views.delete_file_order, name='delete-file-order'),
 ]
