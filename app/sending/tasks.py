a84c82b8b44c85496c1da1619b26b5f206299bbe
Aleksandr140590 <alex2717@yandex.kz>
2023-12-11 22:28:12 +0600

    Update tasks.py

--- app/sending/tasks.py
+++ app/sending/tasks.py
@@ -1,8 +1,14 @@
 from celery import shared_task
+from celery.utils.log import get_task_logger
 from django.utils.module_loading import import_string
+
+logger = get_task_logger(__name__)
 
 
 @shared_task()
 def send_email_notification(sending, context, recipients):
-    notification_class = import_string(sending)(context=context)
-    notification_class.send(recipients)
+    try:
+        notification_class = import_string(sending)(context=context)
+        notification_class.send(recipients)
+    except Exception as er:
+        logger.error(f"Ошибка при отправке письма пользователям {recipients} с ошибкой {er}")
