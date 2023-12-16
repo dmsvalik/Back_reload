f28e99b05ac679c70f635078281b4375c554049c
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-16 13:00:23 +0200

    Mod IsFileOwner permission class: added docstring and created query for filter files from database

--- app/utils/permissions.py
+++ app/utils/permissions.py
@@ -32,8 +32,21 @@ class IsFileExist(permissions.BasePermission):
 
 class IsFileOwner(permissions.BasePermission):
     def has_permission(self, request, view):
+        """
+        Returns True if the file "server path" field
+        contains path from the URL
+        and the file is created by the current user
+        """
         path = view.kwargs['path']
-        file = FileData.objects.get(server_path__contains=path)
-        if not file.user_account == request.user:
+        current_user = request.user
+        file = (
+            OrderFileData.objects
+            .filter(
+                Q(server_path__contains=path) & Q(order_id__user_account=current_user)
+            )
+            .first()
+            )
+
+        if not file.order_id.user_account == current_user:
             return False
         return True
