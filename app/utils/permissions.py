2ff0302931a717d252096d734f7896e0534be3c7
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-16 12:55:41 +0200

    Mod IsContractor permission class: removed query for get file and added docstring
    Mod IsFileExist permission class: changed FileData model to OrderFileData model and added docstring

--- app/utils/permissions.py
+++ app/utils/permissions.py
@@ -1,22 +1,30 @@
+from django.db.models import Q
 from rest_framework import permissions
 
-from app.orders.models import FileData
+from app.orders.models import OrderFileData
 from app.utils import errorcode
 
 
 class IsContactor(permissions.BasePermission):
     def has_permission(self, request, view):
-        path = view.kwargs['path']
-        file = FileData.objects.get(server_path__contains=path)
-        if not request.user.role == 'contractor':
+        """
+        Return True if current user is "contractor"
+        """
+        current_user = request.user
+
+        if not current_user.role == 'contractor':
             return False
         return True
 
 
 class IsFileExist(permissions.BasePermission):
     def has_permission(self, request, view):
+        """
+        Return True if the file is exists
+        """
         path = view.kwargs['path']
-        file_exists = FileData.objects.filter(server_path__contains=path).exists()
+        file_exists = OrderFileData.objects.filter(server_path__contains=path).exists()
+
         if file_exists:
             return True
         raise errorcode.FileNotFound()
