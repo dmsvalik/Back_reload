bd8e6167f2ee6c9c0174cd74bb07b341dc5d826c
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-20 17:30:19 +0200

    rewrite signal for recalculate quota to AuthSignal that will run all jobs for users app

--- app/users/signals.py
+++ app/users/signals.py
@@ -6,39 +6,53 @@ from rest_framework.response import Response
 from app.users.models import UserAccount
 from app.orders.models import OrderModel, OrderFileData
 from app.users.utils import UserQuotaManager
-
+from app.sending.views import send_user_notifications
 
 post_request = Signal()
 
 
-def quota_recalculate(**kwargs):
-    """
-    Пересчитывает квоту пользователя и удаляет ключ из куки
-    При условии что есть ключ, найден заказ по ключю, найдены файлы у заказа
-    Если в вызов сигнала передать ключ "addition" то менеджер будет прибавлять
-    найденные размеры файла, если ключа нет то отнимать
-    """
-    request: Request = kwargs.get("sender")
-    cookie_key: str = request.COOKIES.get("key")
-    user: UserAccount = kwargs.get("user")
+class AuthSignal:
 
-    if cookie_key:
-        order: OrderModel | None = OrderModel.objects.filter(key=cookie_key, user_account__isnull=True).first()
+    def __call__(self, **kwargs):
+        request: Request = kwargs.get("request")
+        addition: bool = bool(kwargs.get("addition", False))
+        response: Response = kwargs.get("response")
+        user: UserAccount = kwargs.get("user")
 
-        if order:
-            order.user_account = user
-            order.save()
-            files: QuerySet[OrderFileData] | None = OrderFileData.objects.filter(order_id__pk=order.pk).all()
-
-            if files:
-                manager = UserQuotaManager(user)
-                if bool(kwargs.get("addition", False)):
-                    manager.add_many(files=files)
-                else:
-                    manager.subtract_many(files=files)
-
-                response: Response = kwargs.get("reponse")
-                response.delete_cookie("key")
+        self._quota_recalcuate(request, addition, user, response)
+        self._user_notify(user)
 
 
-post_request.connect(quota_recalculate)
+    def _quota_recalcuate(self, request: Request, addition: bool, user: UserAccount, response: Response):
+        cookie_key: str = request.COOKIES.get("key")
+
+        if cookie_key:
+            order: OrderModel | None = OrderModel.objects.filter(key=cookie_key, user_account__isnull=True).first()
+
+            if order:
+                order.user_account = user
+                order.save()
+                self.order = order
+
+                files: QuerySet[OrderFileData] | None = OrderFileData.objects.filter(order_id__pk=order.pk).all()
+
+                if files:
+                    manager = UserQuotaManager(user)
+                    if bool(addition):
+                        manager.add_many(files=files)
+                    else:
+                        manager.subtract_many(files=files)
+
+                    response.delete_cookie("key")
+
+
+    def _user_notify(self, user: UserAccount):
+        if hasattr(self, "order") and user.notifications:
+            send_user_notifications(
+            user,
+            "ORDER_CREATE_CONFIRMATION",
+            {"order_name": self.order.name,"user_id": user.id},
+            [user.email]
+            )
+
+post_request.connect(AuthSignal())
