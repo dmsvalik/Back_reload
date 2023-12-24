8173beebe85509b8dd1decdeb9333e880a102026
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-12-24 21:30:36 +0400

    Added name for document_view

--- app/utils/urls.py
+++ app/utils/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("create/all_data/", create_all_data, name="create_all_data"),
 
     path("tasks/<task_id>/", get_task_status, name="get_task_status"),
-    path("documents/<path:path>", document_view),
+    path("documents/<path:path>", document_view, name="document-view"),
 
     path("check_expired_auction_orders/", check_expired_auction_orders),
     path("utils/gallery/", GalleryImagesViewSet.as_view({"get": "list"})),
