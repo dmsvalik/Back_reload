4d12989cc11a6d6407732bcabb7c87c081201d0e
IlyaBulatau <ilyabulatau@gmail.com>
2024-01-23 12:11:21 +0200

    added enum of staties

--- app/orders/constants.py
+++ app/orders/constants.py
@@ -1,16 +1,37 @@
-ORDER_STATE_CHOICES = (
-    ("draft", "Черновик"),
-    ("offer", "Создание предложений"),
-    ("selected", "Исполнитель выбран"),
-    ("completed", "Заказ выполнен"),
-)
+from enum import Enum
+
+
+class OfferState(Enum):
+    PROCESSE = "processed"
+    VIEWED = "viewed"
+    SELECTED = "selected"
+    REJECTED = "rejected"
+    ARCHIVE = "archive"
+
+
+class OrderState(Enum):
+    DRAFT = "draft"
+    OFFER = "offer"
+    SELECTED = "selected"
+    COMPLETED = "completed"
+
 
 OFFER_STATE_CHOICES = (
-    ("processed", "В обработке"),  # при создании оффера
-    ("viewed", "Просмотрен"),  # при просмотре оффера заказчиком
-    ("selected", "Выбран"),  # при выборе оффера
-    ("rejected", "Отклонен"),  # заказчик выбрал другой оффер к заказу
-    ("archive", "В архиве"),  # оффер перенесен в архив
+    (OfferState.PROCESSE.value, "В обработке"),  # при создании оффера
+    (OfferState.VIEWED.value, "Просмотрен"),  # при просмотре оффера заказчиком
+    (OfferState.SELECTED.value, "Выбран"),  # при выборе оффера
+    (
+        OfferState.REJECTED.value,
+        "Отклонен",
+    ),  # заказчик выбрал другой оффер к заказу
+    (OfferState.ARCHIVE.value, "В архиве"),  # оффер перенесен в архив
+)
+
+ORDER_STATE_CHOICES = (
+    (OrderState.DRAFT.value, "Черновик"),
+    (OrderState.OFFER.value, "Создание предложений"),
+    (OrderState.SELECTED.value, "Исполнитель выбран"),
+    (OrderState.COMPLETED.value, "Заказ выполнен"),
 )
 
 
