ebdc119b53f96b916eb10ab28d327ce7aa6b3f89
IlyaBulatau <ilyabulatau@gmail.com>
2024-01-28 22:18:17 +0200

    auth back removed

--- config/settings.py
+++ config/settings.py
@@ -240,8 +240,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
