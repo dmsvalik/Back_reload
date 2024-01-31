3f362cc492c9595ea9b88daa1ac058c227472bc5
IlyaBulatau <ilyabulatau@gmail.com>
2024-01-31 19:47:43 +0200

    ref urls

--- app/orders/urls.py
+++ app/orders/urls.py
@@ -1,9 +1,48 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+router = DefaultRouter()
+router.register("offers", views.OfferViewSet)
 
 urlpatterns = [
+    path(
+        "orders",
+        include(
+            [
+                path(
+                    "last/",
+                    views.get_answers_to_last_order,
+                    name="get-last-order-answers",
+                ),
+                path("create/", views.create_order, name="order-create"),
+                path(
+                    "client/all_orders/",
+                    views.AllOrdersClientViewSet.as_view({"get": "list"}),
+                ),
+                path(
+                    "client/archive/",
+                    views.ArchiveOrdersClientViewSet.as_view({"get": "list"}),
+                ),
+                path(
+                    "file_order/",
+                    views.delete_file_order,
+                    name="delete-file-order",
+                ),
+                path(
+                    "download/<str:file_id>/",
+                    views.get_download_file_link,
+                    name="get-download-link",
+                ),
+                path(
+                    "clone/",
+                    views.CloneOrderView.as_view(),
+                    name="order_clone",
+                ),
+            ]
+        ),
+    ),
     path(
         "orders/<int:pk>/",
         include(
@@ -26,33 +65,5 @@ urlpatterns = [
             ]
         ),
     ),
-    path(
-        "order/last/",
-        views.get_answers_to_last_order,
-        name="get-last-order-answers",
-    ),
-    path(
-        "offers/<int:pk>/",
-        views.OfferViewSet.as_view(
-            {"get": "retrieve", "delete": "destroy", "put": "update"}
-        ),
-    ),
-    path("order/create/", views.create_order, name="order-create"),
-    path(
-        "order/client/all_orders/",
-        views.AllOrdersClientViewSet.as_view({"get": "list"}),
-    ),
-    path(
-        "order/client/archive/",
-        views.ArchiveOrdersClientViewSet.as_view({"get": "list"}),
-    ),
-    path(
-        "order/file_order/", views.delete_file_order, name="delete-file-order"
-    ),
-    path(
-        "order/download/<str:file_id>/",
-        views.get_download_file_link,
-        name="get-download-link",
-    ),
-    path("order/clone/", views.CloneOrderView.as_view(), name="order_clone"),
+    path("", include(router.urls)),
 ]
