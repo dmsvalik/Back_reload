02873fd103be361e2eb78ed24a6e679a5d83e3ba
Vladislav_Poroshin <vld.poroshin@gmail.com>
2024-02-03 19:27:15 +0400

    The method for updating file data is moved to the base class

--- app/orders/utils/clone_db_data.py
+++ app/orders/utils/clone_db_data.py
@@ -13,7 +13,51 @@ from ...users.utils.quota_manager import UserQuotaManager
 
 
 class BaseOrderDB(object):
-    pass
+    def __init__(self, user_id: int = None, order_id: int = None):
+        """
+        Базовый класс для работы с базой данных заказа
+        @param order_id: id заказа
+        @param user_id: id пользователя
+        """
+        self.user_id = user_id
+        self.order_id = order_id
+
+    def update_order_file_path(
+        self, path_to: str, server: bool = True, cloud: bool = True
+    ) -> None:
+        """
+        Метод обновляет пути до файлов в БД, по умолчанию обновляются все
+        пути (сервер, YandexCloud), однако опционально можно отключить.
+        @param path_to: Новый путь до папки с файлами
+        @param server: bool флаг хранилища сервера
+        @param cloud: bool флаг YandexCloud
+        @return: QuerySet обновленных файлов
+        """
+        files = OrderFileData.objects.filter(order_id=self.order_id)
+
+        fields_update = []
+        if server:
+            fields_update.append("server_path")
+            files = files.exclude(server_path="")
+        if cloud:
+            fields_update.append("yandex_path")
+            files = files.exclude(yandex_path="")
+
+        for file in files:
+            if server:
+                file.server_path = os.path.join(
+                    path_to, file.server_path.split("/")[-1]
+                )
+            if cloud:
+                file.yandex_path = os.path.join(
+                    path_to, file.yandex_path.split("/")[-1]
+                )
+
+        OrderFileData.objects.bulk_update(files, fields_update, batch_size=100)
+
+        user = get_user_model().objects.get(pk=self.user_id)
+        quota_manager = UserQuotaManager(user)
+        quota_manager.add_many(files, server=server, cloud=cloud)
 
 
 class CloneOrderDB(BaseOrderDB):
@@ -21,18 +65,15 @@ class CloneOrderDB(BaseOrderDB):
     Класс с набором методов для клонирования информации
     """
 
-    def __init__(
-        self, user_id: int, old_order_id=None, new_order_id=None
-    ) -> None:
+    def __init__(self, user_id: int, old_order_id=None, order_id=None) -> None:
         """
         Класс с набором методов для клонирования информации
         @param old_order_id: id копируемого заказа
-        @param new_order_id: id нового заказа
+        @param order_id: id нового заказа
         @param user_id: id пользователя
         """
+        super().__init__(user_id=user_id, order_id=order_id)
         self.old_order_id = old_order_id
-        self.user_id = user_id
-        self.new_order_id = new_order_id
 
     def clone_order(self) -> int:
         """
@@ -49,7 +90,7 @@ class CloneOrderDB(BaseOrderDB):
         )
         user = get_user_model().objects.get(pk=self.user_id)
         new_order = OrderModel.objects.create(user_account=user, **old_order)
-        self.new_order_id = new_order.pk
+        self.order_id = new_order.pk
         return new_order.pk
 
     def clone_order_question_response(self) -> None:
@@ -61,7 +102,7 @@ class CloneOrderDB(BaseOrderDB):
             order_id=self.old_order_id
         ).values("response", "question_id")
         save_many_obj_to_db(
-            QuestionResponse, responses, order_id=self.new_order_id
+            QuestionResponse, responses, order_id=self.order_id
         )
 
     def clone_order_file_data(self) -> None:
@@ -79,41 +120,16 @@ class CloneOrderDB(BaseOrderDB):
             "yandex_size",
             "server_size",
         )
-        new_order = OrderModel.objects.get(pk=self.new_order_id)
+        new_order = OrderModel.objects.get(pk=self.order_id)
         for file in files_data:
             file["question_id"] = Question.objects.get(pk=file["question_id"])
         save_many_obj_to_db(OrderFileData, files_data, order_id=new_order)
 
-    def update_order_file_path(
-        self, path_to: str, server: bool = True, cloud: bool = True
-    ) -> None:
-        """
-        Метод обновляет пути до файлов в БД, по умолчанию обновляются все
-        пути (сервер, YandexCloud), однако опционально можно отключить.
-        @param path_to: Новый путь до папки с файлами
-        @param server: bool флаг хранилища сервера
-        @param cloud: bool флаг YandexCloud
-        @return: QuerySet обновленных файлов
-        """
-        files = OrderFileData.objects.filter(order_id=self.new_order_id)
-        for file in files:
-            if server:
-                file.server_path = os.path.join(
-                    path_to, file.server_path.split("/")[-1]
-                )
-            if cloud:
-                file.yandex_path = os.path.join(
-                    path_to, file.yandex_path.split("/")[-1]
-                )
 
-        fields_update = []
-        if server:
-            fields_update.append("server_path")
-        if cloud:
-            fields_update.append("yandex_path")
+class UpdateOrderDB(BaseOrderDB):
+    """
+    Класс для обновления данных заказа в БД
+    """
 
-        OrderFileData.objects.bulk_update(files, fields_update, batch_size=100)
-
-        user = get_user_model().objects.get(pk=self.user_id)
-        quota_manager = UserQuotaManager(user)
-        quota_manager.add_many(files, server=server, cloud=cloud)
+    def __init__(self, user_id: int, order_id: int = None) -> None:
+        super().__init__(user_id=user_id, order_id=order_id)
