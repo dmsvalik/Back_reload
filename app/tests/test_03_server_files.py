0cd31190c7c3c394ee4740905eff5d9dabed080b
Vladislav_Poroshin <vld.poroshin@gmail.com>
2024-02-14 16:21:05 +0400

    Created test for ServerFiles and ImageServerFiles

--- app/tests/test_03_server_files.py
+++ app/tests/test_03_server_files.py
@@ -1,11 +1,11 @@
-from unittest.mock import ANY
-
 import pytest
-from app.file.storage.server import TmpServerFiles
+
+from app.file.exception import ThisNotFileError
+from app.file.storage.server import ServerFiles
 from config.settings import FILE_SETTINGS
 
 
-class TestTmpServerFiles:
+class TestServerFiles:
     """Тест-кейс работы с файлами"""
     @pytest.mark.parametrize(
         "filename, result",
@@ -19,7 +19,7 @@ class TestTmpServerFiles:
     )
     def test_get_file_format(self, filename, result):
         """Тест получения формата файла"""
-        server_files = TmpServerFiles()
+        server_files = ServerFiles()
         assert server_files.get_file_format(filename) == result
 
     @pytest.mark.parametrize(
@@ -37,7 +37,58 @@ class TestTmpServerFiles:
     )
     def test_get_unique_filename(self, filename, file_format, length):
         """Тест генерации рандомного имени файла"""
-        server_files = TmpServerFiles()
+        server_files = ServerFiles()
         result = server_files.get_unique_filename(filename)
         assert result[-4:] == file_format
         assert len(result) == length
+
+    @pytest.mark.parametrize(
+        "lst,result", [
+            (["test", "test", "test.jpg"], "test/test/test.jpg"),
+            (["test1", "test1", "test.dox"], "test1/test1/test.dox")
+        ]
+    )
+    def test_generate_path(self, lst, result):
+
+        server_files = ServerFiles()
+        res = server_files.generate_path(*lst)
+        assert res == result
+
+    @pytest.mark.parametrize(
+        "path,result", [
+            ("test/test/test.jpg", "test.jpg"),
+            ("test1/test1/test.dox", "test.dox")
+        ]
+    )
+    def test_get_filename_from_path(self, path, result):
+        server_files = ServerFiles()
+        res = server_files.get_filename_from_path(path)
+        assert res == result
+
+    def test_get_filename_from_path_invalid_path(self):
+        server_files = ServerFiles()
+        with pytest.raises(ThisNotFileError):
+            server_files.get_filename_from_path("test1/test1/ggdgr")
+
+    @pytest.mark.parametrize(
+        "path,filename,result", [
+            ("test/test/test.jpg", "test1.jpg", "test/test/test1.jpg"),
+            ("test/test/test.dox", "test1.jpg", "test/test/test1.jpg"),
+        ]
+    )
+    def test_replace_filename(self, path, filename, result):
+        server_files = ServerFiles()
+        res = server_files.replace_filename_from_path(path, filename)
+        assert res == result
+
+    @pytest.mark.parametrize(
+        "path,filename", [
+            ("test/test/testjpg", "test1.jpg"),
+            ("test/test/test.dox", "test1jpg"),
+        ]
+    )
+    def test_replace_filename_invalid(self, path, filename):
+
+        server_files = ServerFiles()
+        with pytest.raises(ThisNotFileError):
+            server_files.replace_filename_from_path(path, filename)
