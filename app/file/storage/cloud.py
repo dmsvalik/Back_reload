2250e0d3689ea308a2cf6ba0ace4448bd953f888
Vladislav_Poroshin <vld.poroshin@gmail.com>
2024-02-18 22:27:06 +0400

    Added methods for uploading files to the cloud

--- app/file/storage/cloud.py
+++ app/file/storage/cloud.py
@@ -1,13 +1,16 @@
+import json
 import os
 import requests
 
+from .mixin import WorkFilePathMixin
 from config.settings import TOKEN
+from app.file import exception as ex
 
 
-class CloudBase(object):
+class CloudBase(WorkFilePathMixin):
     """Базовый класс для работы с файлами через API Yandex Cloud"""
 
-    def __init__(self, overwrite: bool = True):
+    def __init__(self, path: str = None, overwrite: bool = True):
         self.token = (TOKEN,)
         self.URL = "https://cloud-api.yandex.net/v1/disk/resources"
         self.headers = {
@@ -16,6 +19,7 @@ class CloudBase(object):
             "Authorization": f"OAuth {self.token[0]}",
         }
         self.overwrite = overwrite
+        self.dir_path = self._check_or_create(path) if path else None
         self.dirs = None
 
     def upload(self):
@@ -77,3 +81,42 @@ class CloudBase(object):
             path = str(args[0]) + "/"
 
         return self._check_or_create(path)
+
+    def _get_upload_link(self, path: str):
+        """
+        Метод для создания ссылки для загрузки.
+        """
+        params = {"path": path, "overwrite": self.overwrite}
+        res = requests.get(
+            f"{self.URL}/upload", headers=self.headers, params=params
+        )
+        result = json.loads(res.content)
+        try:
+            return result["href"]
+        except KeyError:
+            raise ex.CloudError(result.get("message"))
+
+    def upload_file(self, dir_path: str, filename: str, file) -> str:
+        if self.dir_path != dir_path:
+            self.dir_path = self._check_or_create(dir_path)
+
+        if not self.dir_path:
+            raise ex.CloudError("Не указана ссылка для загрузки")
+
+        full_path = self.generate_path(self.dir_path, filename)
+        upload_link = self._get_upload_link(full_path)
+        response = requests.put(upload_link, headers=self.headers, data=file)
+
+        if response.status_code == 201:
+            return full_path
+        else:
+            raise ex.CloudError(
+                response.json().get(
+                    "message", "Возникла ошибка при загрузке файла"
+                )
+            )
+
+
+class Cloud(CloudBase):
+    def __init__(self, path: str = None, overwrite: bool = True):
+        super().__init__(path, overwrite)
